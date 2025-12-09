fd7accce5aeaecce01a9dafaa281b68bd52df64f
Liena Grytsyna <grytsynaliena@gmail.com>
2025-12-09 10:59:21 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import './styles/App.scss'
 import { io } from 'socket.io-client'
 
 const fallbackSocketUrl =
-  (typeof window !== 'undefined'
-    ? `${window.location.protocol}//${window.location.hostname}:3001`
-    : 'http://localhost:3001')
+  typeof window !== 'undefined'
+    ? `${window.location.origin}`
+    : 'http://localhost:3001'
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL ?? fallbackSocketUrl
 
